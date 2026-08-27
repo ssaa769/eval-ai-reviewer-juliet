ea71bf983603c0889628b5eef997ab06eb8f8581
orange33315 <2467681055@qq.com>
2026-08-27 18:38:13 +0800

    feat: add test case

---
+++ src/case_25466a40.cpp
@@ -0,0 +1,30 @@
+#include "std_testcase.h"
+#include "CWE121_Stack_Based_Buffer_Overflow__CWE129_fgets_81.h"
+
+namespace CWE121_Stack_Based_Buffer_Overflow__CWE129_fgets_81
+{
+
+void CWE121_Stack_Based_Buffer_Overflow__CWE129_fgets_81_bad::action(int data) const
+{
+    {
+        int i;
+        int buffer[10] = { 0 };
+        /* POTENTIAL FLAW: Attempt to write to an index of the array that is above the upper bound
+        * This code does check to see if the array index is negative */
+        if (data >= 0)
+        {
+            buffer[data] = 1;
+            /* Print the array values */
+            for(i = 0; i < 10; i++)
+            {
+                printIntLine(buffer[i]);
+            }
+        }
+        else
+        {
+            printLine("ERROR: Array index is negative.");
+        }
+    }
+}
+
+}
